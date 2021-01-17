590b160702d604810dbacae36fb3016813702d06
Akti <aktikazmatov@gmail.com>
2021-01-18 01:23:26 +0600

    дабавить

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def homepage (request):
 
 
 def test(request):
-    todo_list = ToDO.objects.all()
+    todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
 
 def second(request):
