f9e8c32746d596dc2a93018eb60af39327dc453c
Akti <aktikazmatov@gmail.com>
2021-01-20 17:22:51 +0600

    added todo form

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from main.models import ToDo, Book
 
 def homepage (request):
@@ -23,6 +23,13 @@ def book(request):
         "books.html",
         {"book_list" : book_list}
     )
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
 
 
 
